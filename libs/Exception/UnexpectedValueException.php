d078073f19cc76d3c9de80354e74941dc8479cc0
Harald Lapp <h.lapp@clipdealer.de>
2020-07-29 12:06:27 +0200

    initial commit UnexpectedValueException

---
+++ libs/Exception/UnexpectedValueException.php
@@ -0,0 +1,15 @@
+<?php
+
+/*
+ * This file is part of the 'octris/parser' package.
+ *
+ * (c) Harald Lapp <harald@octris.org>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Octris\Parser\Exception;
+
+class UnexpectedValueException extends \UnexpectedValueException {
+}
